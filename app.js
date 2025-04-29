f4dd0882688ffabc7e9aebd385f203d0692e8618
FznFxp07 <fernando.goncalves.campos@escola.pr.gov.br>
2025-04-29 19:15:04 -0300

    salvar

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert("Boas-vindas ao jogo do Número Secreto");
 let numeroSecreto = 5;
-prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+
+}
